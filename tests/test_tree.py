2e746690487c7d6a068f075470d2a88414eaa119
Jorge Silva <61012171+scjorge@users.noreply.github.com>
2023-05-16 00:43:13 +0000

    configure pytest

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,3 +1,5 @@
+import json
+
 from tree_workflow.tree import Tree
 
 lst = [
@@ -9,14 +11,14 @@ lst = [
 ]
 
 
-tr = Tree()
+def test_tree_from_list():
+    tr = Tree()
+    tree = tr.tree_from_list(lst)
+    print(json.dumps(tree))
 
 
-import json
-
-tree = tr.tree_from_list(lst)
-print(json.dumps(tree))
-
-
-lista = tr.list_from_tree(tree)
-print(json.dumps(lista))
+def test_list_from_tree():
+    tr = Tree()
+    tree = tr.tree_from_list(lst)
+    lista = tr.list_from_tree(tree)
+    print(json.dumps(lista))
