856f7c77cc9140887a3a8660ecf10a9196a3cfed
Jorge Silva <jorgesilva.ti@hotmail.com>
2023-05-15 23:14:11 -0300

    create flow key test

--- tests/test_tree.py
+++ tests/test_tree.py
@@ -11,10 +11,48 @@ lst = [
 ]
 
 
+final_tree_with_flow_key = [
+    {
+        "id": 1,
+        "parent": 0,
+        "flow": "1",
+        "children": [
+            {
+                "id": 2,
+                "parent": 1,
+                "flow": "1-1",
+                "children": [{"id": 4, "parent": 2, "flow": "1-1-1"}],
+            },
+            {"id": 3, "parent": 1, "flow": "1-2"},
+        ],
+    },
+    {"id": 5, "parent": 0, "flow": "2"},
+]
+
+
+final_tree_without_flow_key = [
+    {
+        "id": 1,
+        "parent": 0,
+        "children": [
+            {"id": 2, "parent": 1, "children": [{"id": 4, "parent": 2}]},
+            {"id": 3, "parent": 1},
+        ],
+    },
+    {"id": 5, "parent": 0},
+]
+
+
+def test_tree_from_list_without_flow_key():
+    trr = Tree(flow=False)
+    tree = trr.tree_from_list(lst)
+    assert tree == final_tree_without_flow_key
+
+
 def test_tree_from_list():
     tr = Tree()
     tree = tr.tree_from_list(lst)
-    print(json.dumps(tree))
+    assert tree == final_tree_with_flow_key
 
 
 def test_list_from_tree():
